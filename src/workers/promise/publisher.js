33f38c1ef1e41e6e7c56569dd343e873f64c3c3a
henrique <henriquehvisa@gmail.com>
2020-04-03 23:07:40 -0300

    refactoring publish and worker in promise type of worker example

--- src/workers/promise/publisher.js
+++ src/workers/promise/publisher.js
@@ -1,21 +1,25 @@
 const amqp = require('amqplib')
 const url = 'amqp://localhost'
 
+const createConnection = async () => {
+  try {
+    return await amqp.connect(url)
+  } catch (err) {
+    throw err
+  }
+}
+
+const createChannel = async conn => {
+  try {
+    return await conn.createChannel()
+  } catch (err) {
+    throw err
+  }
+}
+
 const init = async () => {
-  let conn
-  let channel
-
-  try {
-    conn = await amqp.connect(url)
-  } catch (err) {
-    throw err
-  }
-
-  try {
-    channel = await conn.createChannel()
-  } catch (err) {
-    throw err
-  }
+  const conn = await createConnection()
+  const channel = await createChannel(conn)
 
   const queue = 'queuePromise'
   const message = 'queue with promise'
--- src/workers/promise/worker.js
+++ src/workers/promise/worker.js
@@ -1,21 +1,25 @@
 const amqp = require('amqplib')
 const url = 'amqp://localhost'
 
-const init = async () => {
-  let conn
-  let channel
-
+const createConnection = async () => {
   try {
-    conn = await amqp.connect(url)
-  } catch(err) {
+    return await amqp.connect(url)
+  } catch (err) {
     throw err
   }
+}
 
+const createChannel = async conn => {
   try {
-    channel = await conn.createChannel()
-  } catch(err) {
-    throw(err)
+    return await conn.createChannel()
+  } catch (err) {
+    throw err
   }
+}
+
+const init = async () => {
+  const conn = await createConnection()
+  const channel = await createChannel(conn)
 
   const queue = 'queuePromise'
 
